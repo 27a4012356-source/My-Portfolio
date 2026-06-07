3e26856978c62fb7b5f045d460232692a5c5e89c
27a4012266-manh <27a4012266@hvnh.edu.vn>
2026-06-07 21:41:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,47 @@ document.querySelectorAll('.btn-primary, .btn-secondary').forEach(btn => {
     btn.style.transform = 'translateY(0)';
   });
 });
+document.addEventListener("DOMContentLoaded", () => {
+
+    const progressBars =
+        document.querySelectorAll(".progress-fill");
+
+    const observer =
+        new IntersectionObserver(entries => {
+
+        entries.forEach(entry => {
+
+            if(entry.isIntersecting){
+
+                const bar = entry.target;
+
+                const width =
+                    bar.dataset.width;
+
+                bar.style.width = width;
+            }
+
+        });
+
+    });
+
+    progressBars.forEach(bar => {
+        observer.observe(bar);
+    });
+
+});
+const form = document.getElementById("contactForm");
+
+if(form){
+
+    form.addEventListener("submit", function(e){
+
+        e.preventDefault();
+
+        alert("Cảm ơn bạn đã liên hệ với tôi!");
+
+        form.reset();
+
+    });
+
+}
